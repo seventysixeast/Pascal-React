003796329b744281925e35a1d0cecfbb303f54b2
mangita <vandanachadha@yahoo.com>
2021-03-26 11:02:41 +0530

    client heder changes

--- src/client/layout/clientheader.tsx
+++ src/client/layout/clientheader.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logoUrl from "../../assets/images/logo.png";
 const ClientHeader = () => {
+  let user: any = localStorage.getItem("user");
+  user = JSON.parse(user);
 function logout() {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
 }
-
+ 
   return (
     <nav className="border-bottom border-secondary navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
@@ -26,6 +28,13 @@ function logout() {
         </button>
 
         <div className="navbar-nav navbar-nav-right">
+          <ul className="header-nav header-nav-options">
+            <li className="dropdown hidden-xs">
+              <a className="btn btn-icon-toggle btn-default" href="/">
+                <i className="mdi mdi-home"></i>
+              </a>
+            </li>
+          </ul>
           <ul className="header-nav header-nav-profile">
             <li className="dropdown">
               <a
@@ -35,12 +44,12 @@ function logout() {
               >
                 <img
                   id="_profileimg"
-                  src={logoUrl}
+                  src={process.env.REACT_APP_API_URL+user.ImageUrl}
                   className="borderImgGreen"
                   title="Imap Connected."
                 />
                 <span className="profile-info">
-                  Dinesh Sharma<small>Administrator</small>
+                  {user.UserFirstName} {" "} {user.UserLastName}<small>{user.UserRole}</small>
                 </span>
               </a>
               <ul className="dropdown-menu">
@@ -50,22 +59,6 @@ function logout() {
                 <li>
                   <a href="/app/settings/sociallogins">Social Login</a>
                 </li>
-                <li>
-                  <a href="/app/settings/imapsettings">Imap Settings</a>
-                </li>
-                <li>
-                  <a href="/app/settings/notificationsettings">
-                    Email Settings
-                  </a>
-                </li>
-                <li className="divider"></li>
-
-                <li>
-                  <a href="/app/projects?user=me">My Projects</a>
-                </li>
-
-                <li className="divider"></li>
-
                 <li>
                   <a href="/" onClick={()=>logout()}>
                     <i className="mdi mdi-fw mdi-power-off text-danger"></i> Logout
